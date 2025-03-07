61ae84f2bae9b82c8af613365c8e7c929bca1b2c
Geraldine Xiomar <gxiomarbm@gmail.com>
2025-03-06 20:36:57 -0500

    v0.5

--- program.js
+++ program.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     
     // Inicializar el mapa
-    var mapa = L.map("mapa").setView([LATITUD, LONGITUD], 13); // Reemplaza con las coordenadas de tu barrio
+    var mapa = L.map("mapa").setView([4.567803677123118, -74.06954441256678], 17); // Reemplaza con las coordenadas de tu barrio
 
     // Cargar capa base de OpenStreetMap
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -13,12 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }).addTo(mapa);
 
     // Cargar el polígono desde un archivo GeoJSON
-    fetch("poligono.geojson") // Asegúrate de tener este archivo en la misma carpeta
+    fetch("barrioGranColombia.geojson") // Asegúrate de tener este archivo en la misma carpeta
         .then(response => response.json())
         .then(data => {
             L.geoJSON(data, {
                 style: {
-                    color: "red",
+                    color: "purple",
                     weight: 2,
                     opacity: 0.7
                 }
